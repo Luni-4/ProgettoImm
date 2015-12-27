6490a3cb976266ee0c7322c27d2cfa5fa65658d3
Stefano Pittalis <stefano.pittalis@hotmail.it>
2015-12-27 19:31:57 +0100

    Suddivisione 20x20
    Aggiunta funzione per suddividere immagine in blocchi 20x20

---
+++ Image20x20Subdivider.m
@@ -0,0 +1,40 @@
+function [ImageBlocks] = Image20x20Subdivider(img)
+%% Funzione per la suddivisione dell'immagine di partenza in blocchi 20x20
+
+img = rgb2gray(img);
+img = imresize(img,0.25);
+
+%Recupero dimensioni dell'immagine da analizzare
+[sizeX, sizeY] = size(img);
+
+%Controllo in quanti blocchi posso dividere; se non multiplo di 20,
+%l'ultimo riga e/o colonna avrà dimensioni differenti da 20x20
+numXblocks = sizeX/20;
+numYblocks = sizeY/20; 
+
+%Creo le due matrici per definire il numero di blocchi da 20x20 necessari per le righe e le colonne, 
+%da passare a mat2cell
+dimXblock = ones(floor(numXblocks),1)*20;
+dimYblock = ones(1,floor(numYblocks))*20;
+
+%Se dimensioni immagine immgagine non multiplo di 20, ci sarà un ultima
+%riga e/o colonna più piccola alla fine
+if (round(numXblocks)~=numXblocks)
+    dimXblock(end+ 1, 1) =  (numXblocks - floor(numXblocks))*20 ;
+end
+
+if (round(numYblocks)~=numYblocks)
+    dimYblock(1, end+ 1) =  (numYblocks - floor(numYblocks))*20 ;
+
+end
+
+% Vettorializzo
+X = dimXblock(:);
+Y = dimYblock(:);
+
+% %Suddivido l'immagine in blocchi 20x20, tranne un ipotetica ultima righa o
+% %colonna
+ImageBlocks = mat2cell(img,X,Y);
+
+
+end
