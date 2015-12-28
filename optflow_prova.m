6b1addcd9f2a031a3ab23864411323c1e26cdd65
Luni-4 <Luni-4@users.noreply.github.com>
2015-12-28 13:47:50 +0100

    Function for gaussian and ix and iy

--- optflow_prova.m
+++ optflow_prova.m
@@ -12,16 +12,19 @@
     
     img1 = double(rgb2gray(img1));
     img2 = double(rgb2gray(img2));
-    
-    b = [-1 8 0 -8 1 ]/12;
-   Ix=filter(b,1,img1);
+    exist
+   b = [-1 8 0 -8 1 ]/12;    
    m=b';
-   Iy=filter(m,1,img1);
+   
+   Ix=filter(b,1,img1);  
+   Iy=filter(m,1,img1); 
+   
    It=img1-img2;
+   
    iso=[1 4 6 4 1]/16;
-   Ix=filter(iso,1,Ix);
-   Iy=filter(iso,1,Iy);
-   It=filter(iso,1,It);
+   Ix=filter(iso,1,Ix); 
+   Iy=filter(iso,1,Iy); 
+   It=filter(iso,1,It); 
    
    
 optical_flow = vision.OpticalFlow(...
@@ -33,4 +36,4 @@ flow = step(optical_flow, img1, img2);
 %flow = double(flow);
 u = real(flow);
 v = imag(flow);
-res=(Ix.*u)./Iy-(It./Iy);
+res=(Ix.*u)+(Iy.*v)+It;
