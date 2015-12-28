d4e07d0ab1f9dceac83ed0ee2706268aba5912ba
Luni-4 <Luni-4@users.noreply.github.com>
2015-12-28 14:43:48 +0100

    Update function c

--- Documenti/opticalflow_lk_r_rt.c
+++ Documenti/opticalflow_lk_r_rt.c
@@ -1,46 +1,46 @@
 /*
-*  OPTICALFLOW_LK_Z_RT runtime function for Optical Flow Block
-*  (Lucas & Kanade method). (same as OPTICALFLOW_LK_Z_RT)
+*  OPTICALFLOW_LK_R_RT runtime function for Optical Flow Block
+*  (Lucas & Kanade method).
 *
 *  Copyright 1995-2005 The MathWorks, Inc.
 */
 #include "vipopticalflow_rt.h"  
 
-LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_Z(  const real_T  *inImgA,
-                                       const real_T  *inImgB,
-                                       creal_T *outVel, 
-                                       real_T  *gradCC,
-                                       real_T  *gradRC,
-                                       real_T  *gradRR,
-                                       real_T  *gradCT,
-                                       real_T  *gradRT,
-                                       const real_T  *eigTh,
-                                       int_T  inRows,
-                                       int_T  inCols)
+LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_R( const real32_T  *inImgA,  /* current */
+                                       const real32_T  *inImgB,  /* previous */
+                                       real32_T  *outVel, 
+                                       real32_T  *gradCC,
+                                       real32_T  *gradRC,
+                                       real32_T  *gradRR,
+                                       real32_T  *gradCT,
+                                       real32_T  *gradRT,
+                                       const real32_T  *eigTh,
+                                       int_T   inRows,
+                                       int_T   inCols)
 {
     int_T i, j, ij, mn;
     int_T cFilterHalfLen = 2;
     int_T rFilterHalfLen = 2;
-    int_T BytesPerInCol = sizeof(real_T)*inRows;
+    int_T BytesPerInCol = sizeof(real32_T)*inRows;
 
-    real_T threshEigen      = eigTh[0]; 
-    real_T THRESH_ABS_DELTA = 0; /* delta is the determinant of the 2x2 matrix */
-    real_T THRESH_NORM      = 0;
+    real32_T threshEigen      = eigTh[0]; 
+    real32_T THRESH_ABS_DELTA = 0; /* delta is the determinant of the 2x2 matrix */
+    real32_T THRESH_NORM      = 0;
 
-    real_T gradKernel[5] = {-1/12.0,8/12.0,0,-8/12.0,1/12.0};
+    real32_T gradKernel[5] = {-1/12.0F,8/12.0F,0,-8/12.0F,1/12.0F};
 
-    /* Gaussian separable kernels {1/16,4/16,6/16,4/16,1/16} */
-    real_T gauss1DFilt[5] = {0.0625F,0.25F,0.375F,0.25F,0.0625F};
+    /* Gaussian separable kernels {1/16.0,4/16.0,6/16.0,4/16.0,1/16.0} */
+    real32_T gauss1DFilt[5] = {0.0625F,0.25F,0.375F,0.25F,0.0625F};
 
-    real_T *cFilter;
-    real_T *rFilter;
+    real32_T *cFilter;
+    real32_T *rFilter;
 
     int_T colIdx=0;
-    real_T sum;
+    real32_T sum;
     int_T pixelIdx=0;
 
-    cFilter = (real_T *)&gradKernel[cFilterHalfLen];
-    rFilter = (real_T *)&gradKernel[rFilterHalfLen];
+    cFilter = (real32_T *)&gradKernel[cFilterHalfLen];
+    rFilter = (real32_T *)&gradKernel[rFilterHalfLen];
 
     ij=0;
     mn=0;
@@ -142,32 +142,32 @@ LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_Z(  const real_T  *inImgA,
     }
     for( j = 0; j < inRows*inCols; j++ )
     {
-        real_T tmpGradR = gradRR[j]; 
-        real_T tmpGradC = gradCC[j];
-        real_T tmpGradT = inImgB[j] - inImgA[j]; /* GradT */
+        real32_T tmpGradR = gradRR[j];  // Ix
+        real32_T tmpGradC = gradCC[j]; // Iy
+        real32_T tmpGradT = inImgB[j] - inImgA[j]; /* GradT */   // It
 
-        gradRR[j] = tmpGradR*tmpGradR; 
-        gradCC[j] = tmpGradC*tmpGradC; 
-        gradRC[j] = tmpGradR*tmpGradC; 
+        gradRR[j] = tmpGradR*tmpGradR;  // Ix^2
+        gradCC[j] = tmpGradC*tmpGradC;  // Iy^2
+        gradRC[j] = tmpGradR*tmpGradC;  // Ix*Iy
 
-        gradRT[j] = tmpGradR*tmpGradT; 
-        gradCT[j] = tmpGradC*tmpGradT; 
+        gradRT[j] = tmpGradR*tmpGradT; // Ix*It
+        gradCT[j] = tmpGradC*tmpGradT; // Iy*It
     }
 
     /***********************************************************************************************/		    
     /************** GAUSSIAN FILTERING (TO INTRODUCE WEIGHT) ALONG ROW DIRECTION *******************/
     /***********************************************************************************************/
     mn = 0;
-    cFilter = (real_T *)&gauss1DFilt[cFilterHalfLen];
-    rFilter = (real_T *)&gauss1DFilt[rFilterHalfLen];
+    cFilter = (real32_T *)&gauss1DFilt[cFilterHalfLen];
+    rFilter = (real32_T *)&gauss1DFilt[rFilterHalfLen];
 
     for( colIdx = 0; colIdx < inCols; colIdx++ )
     {
-        real_T *tmpWGradRR = (real_T *)outVel;
-        real_T *tmpWGradCC = tmpWGradRR + inRows;
-        real_T *tmpWGradRC = tmpWGradCC + inRows;
-        real_T *tmpWGradRT = tmpWGradRC + inRows;
-        real_T *tmpWGradCT = tmpWGradRT + inRows;
+        real32_T *tmpWGradRR = &outVel[0];
+        real32_T *tmpWGradCC = &outVel[inRows];
+        real32_T *tmpWGradRC = &outVel[2*inRows];
+        real32_T *tmpWGradRT = &outVel[3*inRows];
+        real32_T *tmpWGradCT = &outVel[4*inRows];
         int_T     colIdxTimesInRows = colIdx*inRows;
 
         memcpy(tmpWGradRR, &gradRR[colIdxTimesInRows],BytesPerInCol);
@@ -279,11 +279,11 @@ LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_Z(  const real_T  *inImgA,
         {
             int_T addr = pixelIdx;
 
-            real_T WWGradRR = 0;
-            real_T WWGradCC = 0;
-            real_T WWGradRC = 0;
-            real_T WWGradRT = 0;
-            real_T WWGradCT = 0;
+            real32_T WWGradRR = 0;
+            real32_T WWGradCC = 0;
+            real32_T WWGradRC = 0;
+            real32_T WWGradRT = 0;
+            real32_T WWGradCT = 0;
 
             for( i = -leftSpace; i <= rightSpace; i++ )
             {
@@ -299,22 +299,26 @@ LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_Z(  const real_T  *inImgA,
             /********************** Solve Linear System *****************************/
             /************************************************************************/
             {
-                real_T delta = (WWGradRC * WWGradRC - WWGradCC * WWGradRR);
-                real_T A = (WWGradCC+WWGradRR)/2.0;
-                real_T tmp  = WWGradCC-WWGradRR;
-                real_T B = 4.0*WWGradRC*WWGradRC + tmp*tmp;
-                real_T sqrtBby2 = sqrt(B)/2.0;
-                real_T eig1=A+sqrtBby2;   /* Largest eigenvalue first  */
-                real_T eig2=A-sqrtBby2;
-                if ((eig1 >= threshEigen) && (eig2 >= threshEigen) && (fabs(delta)>=THRESH_ABS_DELTA))
+                real32_T delta = (WWGradRC * WWGradRC - WWGradCC * WWGradRR);
+                real32_T A = (WWGradCC+WWGradRR)/2.0F;
+                real32_T tmp  = WWGradCC-WWGradRR;
+                real32_T B = 4.0F*WWGradRC*WWGradRC + tmp*tmp;
+                real32_T sqrtBby2 = sqrtf(B)/2.0F;
+                real32_T eig1=A+sqrtBby2;   /* Largest eigenvalue first  */
+                real32_T eig2=A-sqrtBby2;
+                if ((eig1 >= threshEigen) && (eig2 >= threshEigen) && (fabsf(delta)>=THRESH_ABS_DELTA))
                 {
                     /* Solving by Cramer's rule */
-                    real_T deltaC = -(WWGradRT * WWGradRC - WWGradCT * WWGradRR);
-                    real_T deltaR = -(WWGradRC * WWGradCT - WWGradCC * WWGradRT);
-                    real_T Idelta = 1.0 / delta;
+                    real32_T deltaC, deltaC_IDelta;
+                    real32_T deltaR, deltaR_IDelta;
+                    real32_T Idelta = 1.0F / delta;
 
-                    outVel[mn].re = deltaC * Idelta;
-                    outVel[mn].im = deltaR * Idelta;
+                    deltaC = -(WWGradRT * WWGradRC - WWGradCT * WWGradRR);
+                    deltaR = -(WWGradRC * WWGradCT - WWGradCC * WWGradRT);
+
+                    deltaC_IDelta = deltaC * Idelta;
+                    deltaR_IDelta = deltaR * Idelta;
+                    outVel[mn] = deltaC_IDelta*deltaC_IDelta + deltaR_IDelta * deltaR_IDelta;
                     mn++;
                 }
                 else if ((eig1 >= threshEigen) && (eig2 < threshEigen))
@@ -323,36 +327,36 @@ LIBMWVISIONRT_API void MWVIP_OpticalFlow_LK_Z(  const real_T  *inImgA,
                     /* singular system, determinant is non-invertible */
                     /* gradient flow is normalized */
 
-                    real_T tmpRC_CC = WWGradRC + WWGradCC;
-                    real_T tmpRR_RC = WWGradRR + WWGradRC;
-                    real_T norm = tmpRC_CC*tmpRC_CC + tmpRR_RC*tmpRR_RC;
+                    real32_T tmpRC_CC = WWGradRC + WWGradCC;
+                    real32_T tmpRR_RC = WWGradRR + WWGradRC;
+                    real32_T norm = tmpRC_CC*tmpRC_CC + tmpRR_RC*tmpRR_RC;
 
                     if( norm >= THRESH_NORM )
                     {
-                        real_T invNorm = 1.0 / norm;
-                        real_T temp = -(WWGradRT + WWGradCT) * invNorm;
-                        outVel[mn].re = tmpRC_CC * temp;
-                        outVel[mn].im = tmpRR_RC * temp;
+                        real32_T invNorm = 1.0F / norm;
+                        real32_T temp = -(WWGradRT + WWGradCT) * invNorm;
+                        real32_T velReal = tmpRC_CC * temp;
+                        real32_T velImag = tmpRR_RC * temp;
 
+                        outVel[mn] = velReal * velReal + velImag * velImag;
                         mn++;
                     }
                     else
                     {
-                        outVel[mn].re = 0;
-                        outVel[mn].im = 0;
+                        outVel[mn] = 0;
                         mn++;
                     }
                 }
                 else								            
                 {
-                    outVel[mn].re = 0;
-                    outVel[mn].im = 0;
+                    outVel[mn] = 0;
                     mn++;
                 }
+
             }
         }
     }
 
 }
 
-/* [EOF] opticalflow_lk_z_rt.c */
+/* [EOF] opticalflow_lk_r_rt.c */
