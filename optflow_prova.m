e1c8eb0cb48fd34390f5f15f2ff3973e92b67af7
Luni-4 <Luni-4@users.noreply.github.com>
2015-12-30 18:57:12 +0100

    Documento più chiaro su lavoro

--- optflow_prova.m
+++ optflow_prova.m
@@ -12,20 +12,14 @@
     
     img1 = double(rgb2gray(img1));
     img2 = double(rgb2gray(img2));
-    exist
+    
    b = [-1 8 0 -8 1 ]/12;    
    m=b';
    
-   Ix=filter(b,1,img1);  
-   Iy=filter(m,1,img1); 
+   Ix=conv(img1(:)',b,'same');  
+   Iy=conv(img1(:)',m,'same'); 
    
-   It=img1-img2;
-   
-   iso=[1 4 6 4 1]/16;
-   Ix=filter(iso,1,Ix); 
-   Iy=filter(iso,1,Iy); 
-   It=filter(iso,1,It); 
-   
+   It=img2-img1; kmeans
    
 optical_flow = vision.OpticalFlow(...
     'Method', 'Lucas-Kanade', ...
@@ -36,4 +30,4 @@ flow = step(optical_flow, img1, img2);
 %flow = double(flow);
 u = real(flow);
 v = imag(flow);
-res=(Ix.*u)+(Iy.*v)+It;
+res=(Ix.*u(:)')+(Iy.*v(:)')+It(:)';
