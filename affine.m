4233c6a2f550b5716f94f843a1708294782508f7
Luni-4 <Luni-4@users.noreply.github.com>
2016-01-05 23:51:13 +0100

    Trovato vettori affini e semplificato

--- affine.m
+++ affine.m
@@ -1,21 +1,22 @@
-function [Hxi,Hyi] = affine(imgReg,uReg,vReg,sy,soglia,conto)
+function [Hxi,Hyi,uStimato,vStimato] = affine(uReg,vReg)
     sumr=0; 
     sumvx=0;
     sumvy=0;
-    if conto <= soglia*sy
-        rig=20*(soglia-1);
-    end
-    col=20*(mod((conto-1),sy));
-    for x=1:size(imgReg,1)
-        for y=1:size(imgReg,2)
-            regressor = [1 x+rig y+col]';
+    for x=1:size(uReg,1)
+        for y=1:size(uReg,2)
+            regressor = [1 x y]';
             sumr=sumr+(regressor*regressor');
-            sumvx=sumvx+regressor*uReg(x+rig,y+col);
-            sumvy=sumvy+regressor*vReg(x+rig,y+col);            
+            sumvx=sumvx+regressor*uReg(x,y);
+            sumvy=sumvy+regressor*vReg(x,y);            
         end
     end
-    Hxi= sumr\sumvx;
-    Hyi=sumr\sumvy;   
+    Hxi=sumr\sumvx;
+    Hyi=sumr\sumvy;     
+    
+    % Per ogni pixel in regione viene calcolata stima del flusso ottico
+    % lungo le x e le y
+    [rt,ct]=find((uReg==0 | uReg~=0)); % uStimato,vStimatotrovare indici regione 
+    z=[ones([numel(uReg),1]) rt ct]; % Costruzione regressore 
+    uStimato=reshape(z*Hxi,[size(uReg,1),size(uReg,2)]); %Calcolo valori stimati flusso ottico vx
+    vStimato=reshape(z*Hyi,[size(uReg,1),size(uReg,2)]); %Calcolo valori stimati flusso ottico vy
 end
-
-
--- affine1.m
+++ affine1.m
@@ -1,15 +1,33 @@
-function [Hyi,Hxi] = affine1(imgReg,uReg,vReg)
+function [Hxi,Hyi] = affine1(imgReg,uReg,vReg,sy,soglia,conto)
     sumr=0; 
     sumvx=0;
     sumvy=0;
+    if conto <= soglia*sy
+        rig=20*(soglia-1);
+    end
+    col=20*(mod((conto-1),sy));
     for x=1:size(imgReg,1)
         for y=1:size(imgReg,2)
-            regressor = [1 x y]';
+            regressor = [1 x+rig y+col]';
             sumr=sumr+(regressor*regressor');
-            sumvx=sumvx+regressor*uReg(x,y);
-            sumvy=sumvy+regressor*vReg(x,y);            
+            sumvx=sumvx+regressor*uReg(x+rig,y+col);
+            sumvy=sumvy+regressor*vReg(x+rig,y+col);            
         end
     end
-    Hxi=sumr\sumvx;
+    Hxi= sumr\sumvx;
     Hyi=sumr\sumvy;   
 end
+
+    affineRegX = cell(size(imgReg,1),size(imgReg,2));
+    affineRegY = cell(size(imgReg,1),size(imgReg,2));
+    soglia=1; m=0;
+    for i=1:size(imgReg,1)
+        for j=1:size(imgReg,2)
+            m=m+1;
+            [Hxi, Hyi]= affine1(cell2mat(imgReg(i,j)),u,v,size(imgReg,2),soglia,m);
+            affineRegX{i,j}=Hxi;
+            affineRegY{i,j}=Hyi;                     
+        end
+    soglia=soglia+1;
+    end  
+
