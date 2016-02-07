d986c89430884b7bf13e4927a71d34e1e74532ac
Luni-4 <Luni-4@users.noreply.github.com>
2016-02-07 22:17:29 +0100

    Funzione fixata

--- groundtruth.m
+++ groundtruth.m
@@ -13,9 +13,11 @@
     [img2, filenameimg2] = uigetfile('*.pgm;*.PGM;*.ppm;*.PPM', 'Selezionare ground truth');
 
     % Devono essere caricate entrambe le immagini
-    if numel(img1) == 1 || numel(img2) == 1
+    if numel(filenameimg1) == 1 || numel(filenameimg2) == 1
         error('Bisogna caricare entrambe le immagini');
     end
+    
+ 
 
     % Caricamento immagini
     img1 = imread([filenameimg1 img1]);
@@ -27,14 +29,17 @@
 
     % Trovare regioni ground truth
     val = unique(img2);
+    
+    img3 = im2bw(img1);
 
     % Trovare regioni layer
     valer= unique(img1);
     
-    %for i=1:numel(valer)           
-      %      subplot(6,6,i);
+   %for i=1:numel(valer)           
+            %subplot(6,6,i);
            %imshow(img1 == valer(i),[]); 
-      % end    
+          
+    %end    
      
      % Numero di regioni rilevanti (tolti gli 0) in ground truth
      n = (size(val,1)-1);
@@ -42,35 +47,38 @@
     % Definire matrice che contiene layer che hanno in comune pixels con
     % i layer dei ground truth
     livelli = zeros(size(img2,1),size(img2,2),n);
-
-   for lt=1:n
-
+    
+  
+    
+   for lt=1:n       
         currentLayergt = img2 == val(lt+1);      
 
         for i=1:numel(valer)
-            prova = img1 == valer(i);
-            giro = and(prova, currentLayergt);
-            %giro = sum(sum(giro));
-           if sum(sum(giro)) ~= 0
-               livelli(:,:,lt) = (~livelli(:,:,lt) | prova);
-           end
-            %subplot(6,6,i);
-            %imshow(img1 == valer(i),[]); 
+            prova = img1 == valer(i);          
+            giro = and(prova, currentLayergt);          
+           if sum(sum(giro)) ~= 0              
+               livelli(:,:,lt) = (livelli(:,:,lt) + prova;
+           end           
        end    
           
    end
    
-   %Stampare i livelli finali
-   figure(1);
-   imshow(livelli(:,:,1),[]);
-   figure(2);
-   imshow(livelli(:,:,2),[]);
+  
+   
+  % figure(1);
+  % livelli(:,:,i) =  ~(xor(double(img3),livelli(:,:,1)));
+  % imshow(ris1,[]);
+ %  figure(2);
+ %  ris = ~(xor(double(img3),livelli(:,:,2)));
+  % imshow(ris,[]);
+   
    
    % Matrice che salva TP, TN, FP, FN
    matrice = zeros(2,2,n);
    accuracy = zeros(1,1,n);
    
    for i=1:n
+       livelli(:,:,i) =  ~(xor(double(img3),livelli(:,:,i)));
        matrice(1,1,i) = sum(sum(livelli(:,:,1) & img2 == val(i+1)));
        matrice(1,2,i) = sum(sum(livelli(:,:,1) & ~(img2 == val(i+1))));
        matrice(2,1,i) = sum(sum((~livelli(:,:,1)) & ~(img2 == val(i+1))));
