301700516b09a8ababc6230302f822b78166b6cc
Luni-4 <Luni-4@users.noreply.github.com>
2016-02-19 22:28:44 +0100

    Commenti ulteriori

--- MotionSegmentation.m
+++ MotionSegmentation.m
@@ -1,11 +1,24 @@
-% Utente sceglie se usare video esistente o crearlo da frame separati
+% File principale del progetto. Alla fine della sua esecuzione, saranno
+% restituiti, sotto forma di immagini a livello di grigio, i layer di movimento
+% trovati per ogni coppia di frame contenuta nella sequenza video
+% considerata
+
+
+% Utente sceglie se usare video esistente o crearne uno nuovo, partendo da
+% una sequenza di immagini scelta
 in = input('\nPer utilizzare video esistente(.avi), inserire 1.\nPer creare video a partire da frame separati, inserire 2.\n');
 
+% Se il valore contenuto nella variabile in è uguale a 1, utente sceglie
+% quale video caricare. Se il valore è pari a 2, video viene creato da
+% sequenza di immagini. Se il valore inserito è un altro, viene invocato
+% errore
 if (in == 1)
+    % Funzione uigetfile restituisce, per il video scelto, filename e
+    % percorso
     [filename, pathname] = uigetfile( {'*.avi'},'Scegli un video:');
     
 elseif (in == 2)
-    
+    % Viene invocata funzione createVideo
     filename = createVideo;
     pathname = 'videos/';
     
@@ -14,39 +27,12 @@ else
     
 end
 
-%% Inserimento parametri di treshold specifici per ogni video
-% Fare riferimento a tabella documentazione per valori consigliati
-% thLK = input('Inserire il parametro di treshold per Lucas-Kanade\n(Per valori consigliati fare riferimeno tabella documentazione.)\n');
-%
-% if thLK>0
-%
-% else
-%     error('Inserire un valore maggiore di 0.')
-% end
-%
-% thKmeans = input('Inserire il parametro di treshold per Lucas-Kanade\n(Per valori consigliati fare riferimeno tabella documentazione.)\n');
-%
-% if thKmeans>0
-%
-% else
-%     error('Inserire un valore maggiore di 0.')
-% end
-
-%%
-
-% Nome del file video che si vuole creare/aprire
-% filename = 'videos/cats01.avi';
-
-% Se non esiste il video formato dalla sequenza di immagini, crearlo
-% if ~exist(filename,'file')
-%     createVideo(filename);
-% end
-
-% Apertura video in lettura
+% Apertura del file video in lettura
 video = VideoReader([pathname,filename]);
 
-% Definizione di flusso ottico usando Lucas-Kanadae e impostazione soglia
-% di noise
+% opticFlow definisce la struttura usata per il calcolo del flusso ottico usando il metodo Lucas-Kanadae
+% Viene impostata una soglia di noise per evitare di considerare piccoli
+% movimenti, non esistenti nelle coppie di frame analizzate, ma introdotti dal noise 
 opticFlow = opticalFlowLK('NoiseThreshold',0.0001);
 
 % Lettura primo frame viclearene posta fuori dal ciclo per evitare che
@@ -63,12 +49,10 @@ frameGrayCurrent = rgb2gray(frameRGBCurrent);
 % nera)
 flow = estimateFlow(opticFlow, frameGrayCurrent);
 
-% Salvataggio frame corrente da usare in iterazione successiva per
-% funzione di warping
-framePrevious = frameGrayCurrent;
-
-%Variabile che definisce se è prima iterazione AffineMotion sui due frame
-%iniziali
+% Variabile booleana usata per fare in modo che la funzione AffineMotion suddivida il flusso ottico, calcolato
+% tra il primo e il secondo frame del video, in regioni non sovrapposte di
+% dimensioni 20x20. Alla fine della prima iterazione, viene posta a
+% false
 prima = true;
 
 % Contatore frame
@@ -85,6 +69,8 @@ while hasFrame(video)
     
     % Calcolo del flusso ottico tra frame corrente e frame precedente
     flow = estimateFlow(opticFlow, frameGrayCurrent);
+    
+    % Possibilità di stampare il flusso ottico per ogni iterazione
     %         figure(1);
     %         imshow(frameGrayCurrent,[]);
     %         figure(1);
@@ -92,32 +78,44 @@ while hasFrame(video)
     %         flows = opticalFlow(flipud(flow.Vx),flipud(flow.Vy));
     %         plot(flows, 'DecimationFactor',[1 1],'ScaleFactor', 1);
     
+    % Se si è alla prima iterazione, regioniIn è un array vuoto
     if prima == true;
         regioniIn = [];
         
     end
     
-    % Modelli di movimento calcolati da funzione AffineMotion
-    [regioniOut, regioniIn] = AffineMotion(flow.Vx, flow.Vy, prima, regioniIn);
+     % Calcolo dei layer di movimento per ciascuna coppia di frame della sequenza invocando AffineMotion
+     % Vengono passati in input i flussi ottici calcolati precedentemente,
+     % per le x e le y, la variabile prima e la matrice contenente i layer
+     % di movimento trovati all'iterazione precedente. 
+     % Il primo valore restituito contiene i layer di movimento calcolati
+     % per la coppia di frame considerata, il secondo valore le regioni
+    [regioniOut, regioniIn] = AffineMotion(flow.Vx, flow.Vy, prima, regioniIn);   
+
     
-    
-    % Finita prima iterazione, variabile logica prima viene posta a false
-    prima = false;
-    
-    %Salvataggio delle regioni
+    % Se non esiste la directory per il
+    %salvataggio dei layer di movimento, crearla
     if ~exist(['output/', filename],'dir')
         mkdir(['output/',filename]);
     end
+    
+    % I layer di movimento vengono normalizzati (livellì di intensità
+    % compresi tra 0 e 1). Procedimento fatto per facilitare il salvataggio
     regioniNorm =regioniOut/max(abs(regioniOut(:)));
+    
+    % Ad ogni iterazione, vengono salvate le immagini in formato png contenenti i layer di movimento, per ciascuna coppia di frame considerata 
+    % Il filename è strutturato in maniera tale che si possa individuare a
+    % quale coppia ci si riferisce e il numero di layers individuati
     imwrite(regioniNorm, ['output/cats01/Frame ', num2str(frame), '-',num2str(frame+1),' (Cluster=', num2str(numel(unique(regioniOut))),  ').png'],'png');
+   
+    % Incrementare il contatore dei frame
     frame = frame +1;
     
+    % Variabile prima viene posta a false    
+    prima = false;
+    
+    % Segnale sonoro che identifica la fine dell'iterazione
     beep;
-    
-    
-    %     % Salvataggio frame corrente da usare in iterazione successiva per
-    %     % funzione di warping
-    %     regioniIn = regioniOut;
 end
-
+ % Segnale sonoro che identifica la fine dell'esecuzione del processo
 beep;
--- createVideo.m
+++ createVideo.m
@@ -1,18 +1,28 @@
 function filename = createVideo
 
-%
-if nargin<1
-    filename = input('\nInserisci nome video: ','s');
+% La funzione crea un video partendo da una sequenza di immagini scelte da
+% utente
+
+% Utente inserisce nome del video che vuole creare
+filename = input('\nInserisci nome video: ','s');
+
+% Se utente non inserisce nessun filename, funzione restituisce errore
+if isempty(filename)
+    error('Deve essere scelto un filename per il video');
 end
 
-% Funzione per la scelta della serie di immagini
+
+% Viene invocata la funzione pickImages, permette ad un utente di scegliere la sequenza
+% di immagini
 imgnames = pickImages();
 
+% Se imgnames non è un array di celle, perché utente non ha inserito
+% nessuna immagine, funzione restituisce errore
 if ~iscell(imgnames)
     error('Inserire delle immagini');
 end
 
-% Creazione video, specificando filename e formato di compressione
+% Creazione del video, specificando filename e formato di compressione
 video = VideoWriter(['videos/',filename],'Uncompressed AVI');
 
 % Apertura del file video in scrittura
@@ -21,26 +31,30 @@ open(video);
 % Lettura prima immagine della sequenza
 img = imread(imgnames{1});
 
-% Estrazione delle dimensioni
+% Dimensioni dell'immagine di partenza
 dimX= size(img,1);
 dimY = size(img,2);
 
-% Scrittura della prima immagine nel video
+% Scrittura della prima immagine all'interno del file video
 writeVideo(video,img);
 
+% Ciclo effettuato sul resto delle immagini della sequenza
 for i=2:size(imgnames,2)
     
-    % Lettura della i-esima immagine nell' array di celle
+    % Lettura della i-esima immagine, contenuta nell'array di celle
+    % imgnames
     img = imread(imgnames{i});
     
-    % Aggiunta di immagini al video solo se hanno stessa dimensione
+    %  Le immagini sono aggiunte al video solo se hanno la stessa dimensione
     if size(img,1) == dimX && size(img,2) == dimY
         writeVideo(video,img);
     end
 end
 
-% Chiusura file video in scrittura
+% Chiusura del file video in scrittura
 close(video);
 
+% La varaibile restituita dalla funzione contiene il filename scelto da utente e
+% come estensione il formato di compressione 
 filename = [filename, '.avi'];
 end
